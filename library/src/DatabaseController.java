6a7fd6e7bd76e1ef62a1b73f56d482964d204aab
kylepeart21 <kylepeart17@gmail.com>
2025-03-22 13:29:11 -0500

    minor chamge to databaseController

--- library/src/DatabaseController.java
+++ library/src/DatabaseController.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseController {
     private static final String URL = "jdbc:mysql://localhost:3306/LibraryDB";
     private static final String USER = "root";
-    private static final String PASSWORD = "061502kp";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         try {
